def2500cb2968ee31a12f4928bdb976a731af31f
niwach <rappe07@gmail.com>
2018-11-27 23:31:12 +0900

    second commit

---
+++ precache-manifest.da9115b6f69ce44cc43706fd5a7bf23d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e0a597fa6ff2e604704",
+    "url": "/static/js/main.9e0a597f.chunk.js"
+  },
+  {
+    "revision": "ab12b86f7711cc9d0fa3",
+    "url": "/static/js/1.ab12b86f.chunk.js"
+  },
+  {
+    "revision": "9e0a597fa6ff2e604704",
+    "url": "/static/css/main.fce5a541.chunk.css"
+  },
+  {
+    "revision": "76bfc1cef3929ad397e65ecdaca4a726",
+    "url": "/index.html"
+  }
+];

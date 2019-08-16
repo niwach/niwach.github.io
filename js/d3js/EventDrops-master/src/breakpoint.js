db694688002ad0083fb6b78c97ee1736847a909a
niwach <rappe07@gmail.com>
2019-08-16 11:47:15 +0900

    delete all
    delete all

--- js/d3js/EventDrops-master/src/breakpoint.js
+++
@@ -1,9 +0,0 @@
-export const getBreakpointLabel = (breakpoints, point) => {
-    for (const label in breakpoints) {
-        if (point <= breakpoints[label]) {
-            return label;
-        }
-    }
-
-    return 'extra';
-};
